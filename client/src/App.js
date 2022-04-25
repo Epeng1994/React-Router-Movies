26018ff5effa28afe62918255f177ed373c2704c
unknown <epeng1994@gmail.com>
2022-04-25 16:05:40 -0400

    MVP completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Route, Link, Switch } from 'react-router-dom';
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList';
 
 import SavedList from './Movies/SavedList';
+
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -14,6 +18,8 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          console.log(response.data)
+          setMovieList(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -27,10 +33,16 @@ export default function App () {
   };
 
   return (
-    <div>
-      <SavedList list={[ /* This is stretch */]} />
+      <div>
+        <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
-    </div>
+        <Switch>
+          <Route exact path = '/'>
+            <MovieList movies = {movieList}/>
+          </Route>
+          <Route path = '/movies/:id' component={Movie}/>
+        </Switch>
+     
+      </div>
   );
 }
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import {Route, Link, useRouteMatch} from 'react-router-dom'
 
 export default function SavedList(props) {
+  const {path, url} = useRouteMatch();
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map(movie => (
         <span className="saved-movie">{movie.title}</span>
       ))}
-      <div className="home-button">Home</div>
+      <Link to={`${url}`}>
+        <div className="home-button">Home</div>
+      </Link>
     </div>
   );
 }
